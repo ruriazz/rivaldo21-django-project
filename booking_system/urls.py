208a13a5de1fa80f985d0becd20bba77af521f7b
rivaldo guterres <rj142872@gmail.com>
2024-11-20 00:17:35 +0900

    Update message here

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from bookingsystem import views  # Impor fungsi dashboard dari views.py
 
-router = DefaultRouter()
-router.register(r'bookings', BookingViewSet, basename='booking')
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),          # Halaman admin
     path('api/', include('bookingsystem.urls')),  # Semua endpoint API
