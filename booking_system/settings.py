ed8800351b6f9ba7d490166c6942b87bb82b7ccd
rivaldo guterres <rj142872@gmail.com>
2024-11-19 09:55:52 +0900

    Added Render domain to ALLOWED_HOSTS

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6n(%q(^p(-oydfsur-x343cn74r7^v$@y8q3&8_#q=0qk^j^i#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-project-2-umgw.onrender.com']
+ALLOWED_HOSTS = ['django-project-2-umgw.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
