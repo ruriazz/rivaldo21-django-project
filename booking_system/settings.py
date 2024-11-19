1a3da13f0a093ac799ee9f0566347a54f29cf87e
rivaldo guterres <rj142872@gmail.com>
2024-11-19 12:01:17 +0900

    Set DEBUG to True for debugging

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6n(%q(^p(-oydfsur-x343cn74r7^v$@y8q3&8_#q=0qk^j^i#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-project-2-umgw.onrender.com', '127.0.0.1', 'localhost']
 
