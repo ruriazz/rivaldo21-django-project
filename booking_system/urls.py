38566c0b5a3e8d7626ee9750bae087c534dfb6ac
rivaldo guterres <rj142872@gmail.com>
2024-12-02 14:25:38 +0900

    Update project and ignore unnecessary files

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from bookingsystem import views  # Impor fungsi dashboard dari views.py
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),          # Halaman admin
-    path('api/', include('bookingsystem.urls')),  # Semua endpoint API
-    path('', views.dashboard, name='dashboard'), # Path kosong diarahkan ke dashboard
+    path('admin/', admin.site.urls),           # URL admin
+    path('api/', include('bookingsystem.urls')),  # URL API
+    path('', views.dashboard, name='dashboard'),  # Root URL diarahkan ke dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),  # URL /dashboard diarahkan ke dashboard
 ]
